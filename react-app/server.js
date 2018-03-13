c0a5f4c23f5dd82fe45e41745fae8f30b34cabd3
Sh0ri <quent.dasilva@gmail.com>
2018-03-13 16:04:59 +0100

    server connected to mongodb

--- react-app/server.js
+++ react-app/server.js
@@ -1,8 +1,17 @@
-const express = require('express');
-///////////////////////////////////////////////////////////////////EXPRESS///////////////////////////////////////////////////////////////////////////
+var express = require('express')
+  , app = express()
 
+var db = require('./db')
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// Connect to Mongo on start
+db.connect('mongodb://localhost:27017/OTTAWA', function(err) {
+  if (err) {
+    console.log('Unable to connect to Mongo.')
+    process.exit(1)
+  } else {
+    app.listen(3000, function() {
+    	console.log("Connected to the db server");
+      console.log('Listening on port 3000...')
+    })
+  }
+})
